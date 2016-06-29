82396bd96033482b5646cfa81d90924e4019b3d0
gmdev86 <gmdev86@gmail.com>
2016-06-28 22:19:27 -0400

    adding project

--- game/main.py
+++ game/main.py
@@ -20,10 +20,28 @@ class Vector2(object):
         self.x /= magnitude
         self.y /= magnitude
 
+    # rhs stands for Right Hand Side
+    def __add__(self, rhs):
+        return Vector2(self.x + rhs.x, self.y + rhs.y)
+
+    def __sub__(self, rhs):
+        return Vector2(self.x - rhs.x, self.y - rhs.y)
+
+    def __neg__(self):
+        return Vector2(-self.x, -self.y)
+
+    def __mul__(self, scalar):
+        return Vector2(self.x * scalar, self.y * scalar)
+
+    def __div__(self, scalar):
+        return Vector2(self.x / scalar, self.y / scalar)
+
 A = (10.0, 20.0)
 B = (30.0, 35.0)
 AB = Vector2.from_points(A, B)
 print "Vector AB is", AB
-print "Magnitude of Vector AB is", AB.get_magnitude()
-AB.normalize()
-print "Vector AB normalized is", AB
+print "AB * 2 is", AB * 2
+print "AB / 2 is", AB / 2
+#print "AB + (-10, 5) is", AB + (-10, 5)
+#print "Magnitude of AB is", AB.get_magnitude()
+#print "AB normalized is", AB.get_normalized()
